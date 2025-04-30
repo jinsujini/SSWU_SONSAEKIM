2ea3814a4af96dfd633f871c936dd5ba1c516285
sujin <csujin2040@gmail.com>
2025-05-01 08:34:09 +0900

    setup

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cookiParser = require('cookie-parser');
+const session = require('express-session');
+
+const path = require("path");
+
+const app = express();
+app.set('port',process.env.PORT || 3000);
+
+app.use('/',express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cookiParser('mySecretKey'));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'mySecretKey',
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hello, Express');
+});
+
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'),'번 포트에서 대기 중');
+});

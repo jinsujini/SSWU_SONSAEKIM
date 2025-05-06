7ae1d0e227f098ea2330442b1e8297415c2f9eac
yyen <brenda1217@naver.com>
2025-05-06 11:48:51 +0900

    따라하기

--- routers/viewRouters.js
+++ routers/viewRouters.js
@@ -5,4 +5,24 @@ router.get('/mypage', (req, res) => {
     res.render('mypage'); 
 });
 
+router.get('/study/imitate', (req, res) => {
+    res.render('imitate'); 
+});
+
+router.get('/study/imitate/vowel', (req, res) => {
+    res.render('vowel'); 
+});
+
+router.get('/study/imitate/consonant', (req, res) => {
+    res.render('consonant'); 
+});
+
+router.get('/study/imitate/vowel/result', (req, res) => {
+    res.render('vowelResult'); 
+});
+
+router.get('/study/imitate/consonant/result', (req, res) => {
+    res.render('consonantResult'); 
+});
+
 module.exports = router;

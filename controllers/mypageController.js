ee3fcba144c4ac7164f3e3aafb4fc86d3ce35c98
sujin <csujin2040@gmail.com>
2025-06-06 00:09:53 +0900

    #70 [FIX] : 홈화면 css 수정

--- controllers/mypageController.js
+++ controllers/mypageController.js
@@ -3,7 +3,7 @@ const mypageService = require('../services/mypageService');
 exports.renderMypage = mypageService.renderMypage;
 exports.updateProfile = mypageService.updateProfile;
 exports.goMypage = mypageService.goMypage;
-exports.renderNoUser = (req, res) => res.render('mypage/nouser');
+exports.renderNoUser = (req, res) => res.render('auth/home');
 exports.sendEmailCode = mypageService.sendEmailCode;
 exports.verifyEmailCode = mypageService.verifyEmailCode;
 exports.updateEmail = mypageService.updateEmail;

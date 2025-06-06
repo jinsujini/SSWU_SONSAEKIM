334f182aaf34bb9be267bd13a7235f62a3535269
yyen <brenda1217@naver.com>
2025-06-07 01:14:43 +0900

    #65 #77 [FEAT] 따라하기 결과 & 넘버링

--- public/js/imitate/start.js
+++ public/js/imitate/start.js
@@ -24,14 +24,14 @@ async function checkCameraPermission() {
       if (hasPermission) {
         // 권한 있으면 바로 다음 페이지로 이동
         const type = startBtn.dataset.type;
-        window.location.href = `/imitate/study?type=${type}`;
+        window.location.href = `/imitate/${type}/study`;
       } else {
         try {
           // 권한 요청
           const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
           stream.getTracks().forEach(track => track.stop()); // 권한만 확인하려고 스트림 바로 정지
           const type = startBtn.dataset.type;
-          window.location.href = `/imitate/redirect?type=${type}`;
+          window.location.href = `/imitate/${type}/study`;
         } catch (err) {
           alert('카메라 권한이 필요합니다. 권한을 허용해주세요.');
         }
